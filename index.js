1f6c92626e319bf7db8746b88ae513dec867b636
Boris Joffe <boris@joff3.com>
2016-07-18 11:04:38 -0700

    set up webpack further, and create simple react component

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ import h from 'react-hyperscript';
 
 console.log('hello');
 
-const App = h('h1', 'hello');
+const App = React.createClass({
+	getInitialState: function () {
+		return { counter: 0 };
+	},
 
-ReactDOM.render(App, document.getElementById('app'));
+	handleClick: function () {
+		this.setState({counter: this.state.counter + 1});
+	},
+
+	render: function() {
+		return h('div', [
+			h('h1', {onClick: this.handleClick}, 'hello'),
+			h('p', 'counter is: ' + this.state.counter),
+		]);
+	},
+
+});
+
+
+ReactDOM.render(h(App), document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ module.exports = {
 	entry: './index.js',
 	output: {
 		path: 'build',
+		publicPath: '/build/',
 		filename: 'bundle.js',
 	},
 	module: {
@@ -12,6 +13,9 @@ module.exports = {
 			query: {
 				cacheDirectory: true,
 				presets: ['es2015', 'react'],
+				development: {
+					presets: [''],
+				},
 				plugins: ['transform-runtime'],
 			},
 		}],
